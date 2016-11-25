90cf1dfd9800dd2e9d5384a1b446f44f9d6eff04
TRsiew <trsiew@hotmail.com>
2016-11-25 19:33:37 +0800

    init

---
+++ MiniGame_1/Assets/Script/PlayerLibrary.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Player function class
+/// </summary>
+public class PlayerLibrary : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void move(float speed)
+	{
+		
+	}
+}
---
+++ MiniGame_1/Assets/Script/PlayerVariable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerVariable : MonoBehaviour{
+
+	public enum PlayerStatus
+	{
+		idle,move,attack,rest,dead
+	}
+	public PlayerStatus _playerStatus = PlayerStatus.idle;
+
+	public float moveSpeed =5;
+	public float attackSpeed =5;
+	public float attackRestTime=5;
+
+
+}
